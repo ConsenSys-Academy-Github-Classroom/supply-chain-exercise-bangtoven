aeefa1fc7a64270bdd96439a486c3f8f5aadae92
Jungho Bang <jungho.bang@coinbase.com>
2021-10-06 22:37:41 -0700

    should add an item with the provided name and price

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -7,7 +7,7 @@ contract SupplyChain {
 
   uint public skuCount;
 
-  // <items mapping>
+  mapping (uint => Item) items;
 
   enum State { ForSale, Sold, Shipped, Received }
 
@@ -24,14 +24,11 @@ contract SupplyChain {
    * Events
    */
 
-  // <LogForSale event: sku arg>
-
-  // <LogSold event: sku arg>
-
-  // <LogShipped event: sku arg>
-
-  // <LogReceived event: sku arg>
-
+  event LogForSale(uint sku);
+  event LogSold(uint sku);
+  event LogShipped(uint sku);
+  event LogReceived(uint sku);
+  
 
   /* 
    * Modifiers
@@ -77,24 +74,18 @@ contract SupplyChain {
   }
 
   function addItem(string memory _name, uint _price) public returns (bool) {
-    // 1. Create a new item and put in array
-    // 2. Increment the skuCount by one
-    // 3. Emit the appropriate event
-    // 4. return true
-
-    // hint:
-    // items[skuCount] = Item({
-    //  name: _name, 
-    //  sku: skuCount, 
-    //  price: _price, 
-    //  state: State.ForSale, 
-    //  seller: msg.sender, 
-    //  buyer: address(0)
-    //});
-    //
-    //skuCount = skuCount + 1;
-    // emit LogForSale(skuCount);
-    // return true;
+    items[skuCount] = Item({
+     name: _name, 
+     sku: skuCount, 
+     price: _price, 
+     state: State.ForSale, 
+     seller: msg.sender, 
+     buyer: address(0)
+    });
+    
+    skuCount = skuCount + 1;
+    emit LogForSale(skuCount);
+    return true;
   }
 
   // Implement this buyItem function. 
@@ -125,15 +116,15 @@ contract SupplyChain {
   function receiveItem(uint sku) public {}
 
   // Uncomment the following code block. it is needed to run tests
-  /* function fetchItem(uint _sku) public view */ 
-  /*   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) */ 
-  /* { */
-  /*   name = items[_sku].name; */
-  /*   sku = items[_sku].sku; */
-  /*   price = items[_sku].price; */
-  /*   state = uint(items[_sku].state); */
-  /*   seller = items[_sku].seller; */
-  /*   buyer = items[_sku].buyer; */
-  /*   return (name, sku, price, state, seller, buyer); */
-  /* } */
+  function fetchItem(uint _sku) public view 
+    returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) 
+  {
+    name = items[_sku].name;
+    sku = items[_sku].sku;
+    price = items[_sku].price;
+    state = uint(items[_sku].state);
+    seller = items[_sku].seller;
+    buyer = items[_sku].buyer;
+    return (name, sku, price, state, seller, buyer);
+  }
 }
--- test/supply_chain.test.js
+++ test/supply_chain.test.js
@@ -158,187 +158,188 @@ contract("SupplyChain", function (accounts) {
       });
     });
 
-    // describe("Use cases", () => {
-    //   it("should add an item with the provided name and price", async () => {
-    //     await instance.addItem(name, price, { from: alice });
+    describe("Use cases", () => {
+      it("should add an item with the provided name and price", async () => {
+        await instance.addItem(name, price, { from: alice });
 
-    //     const result = await instance.fetchItem.call(0);
+        const result = await instance.fetchItem.call(0);
 
-    //     assert.equal(
-    //       result[0],
-    //       name,
-    //       "the name of the last added item does not match the expected value",
-    //     );
-    //     assert.equal(
-    //       result[2].toString(10),
-    //       price,
-    //       "the price of the last added item does not match the expected value",
-    //     );
-    //     assert.equal(
-    //       result[3].toString(10),
-    //       SupplyChain.State.ForSale,
-    //       'the state of the item should be "For Sale"',
-    //     );
-    //     assert.equal(
-    //       result[4],
-    //       alice,
-    //       "the address adding the item should be listed as the seller",
-    //     );
-    //     assert.equal(
-    //       result[5],
-    //       emptyAddress,
-    //       "the buyer address should be set to 0 when an item is added",
-    //     );
-    //   });
+        assert.equal(
+          result[0],
+          name,
+          "the name of the last added item does not match the expected value"
+        );
+        assert.equal(
+          result[2].toString(10),
+          price,
+          "the price of the last added item does not match the expected value"
+        );
+        assert.equal(
+          result[3].toString(10),
+          SupplyChain.State.ForSale,
+          'the state of the item should be "For Sale"'
+        );
+        assert.equal(
+          result[4],
+          alice,
+          "the address adding the item should be listed as the seller"
+        );
+        assert.equal(
+          result[5],
+          emptyAddress,
+          "the buyer address should be set to 0 when an item is added"
+        );
+      });
 
-    //   it("should emit a LogForSale event when an item is added", async () => {
-    //     let eventEmitted = false;
-    //     const tx = await instance.addItem(name, price, { from: alice });
+      //   it("should emit a LogForSale event when an item is added", async () => {
+      //     let eventEmitted = false;
+      //     const tx = await instance.addItem(name, price, { from: alice });
 
-    //     if (tx.logs[0].event == "LogForSale") {
-    //       eventEmitted = true;
-    //     }
+      //     if (tx.logs[0].event == "LogForSale") {
+      //       eventEmitted = true;
+      //     }
 
-    //     assert.equal(
-    //       eventEmitted,
-    //       true,
-    //       "adding an item should emit a For Sale event",
-    //     );
-    //   });
+      //     assert.equal(
+      //       eventEmitted,
+      //       true,
+      //       "adding an item should emit a For Sale event",
+      //     );
+      //   });
 
-    //   it("should allow someone to purchase an item and update state accordingly", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     var aliceBalanceBefore = await web3.eth.getBalance(alice);
-    //     var bobBalanceBefore = await web3.eth.getBalance(bob);
+      //   it("should allow someone to purchase an item and update state accordingly", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     var aliceBalanceBefore = await web3.eth.getBalance(alice);
+      //     var bobBalanceBefore = await web3.eth.getBalance(bob);
 
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
 
-    //     var aliceBalanceAfter = await web3.eth.getBalance(alice);
-    //     var bobBalanceAfter = await web3.eth.getBalance(bob);
+      //     var aliceBalanceAfter = await web3.eth.getBalance(alice);
+      //     var bobBalanceAfter = await web3.eth.getBalance(bob);
 
-    //     const result = await instance.fetchItem.call(0);
+      //     const result = await instance.fetchItem.call(0);
 
-    //     assert.equal(
-    //       result[3].toString(10),
-    //       SupplyChain.State.Sold,
-    //       'the state of the item should be "Sold"',
-    //     );
+      //     assert.equal(
+      //       result[3].toString(10),
+      //       SupplyChain.State.Sold,
+      //       'the state of the item should be "Sold"',
+      //     );
 
-    //     assert.equal(
-    //       result[5],
-    //       bob,
-    //       "the buyer address should be set bob when he purchases an item",
-    //     );
+      //     assert.equal(
+      //       result[5],
+      //       bob,
+      //       "the buyer address should be set bob when he purchases an item",
+      //     );
 
-    //     assert.equal(
-    //       new BN(aliceBalanceAfter).toString(),
-    //       new BN(aliceBalanceBefore).add(new BN(price)).toString(),
-    //       "alice's balance should be increased by the price of the item",
-    //     );
+      //     assert.equal(
+      //       new BN(aliceBalanceAfter).toString(),
+      //       new BN(aliceBalanceBefore).add(new BN(price)).toString(),
+      //       "alice's balance should be increased by the price of the item",
+      //     );
 
-    //     assert.isBelow(
-    //       Number(bobBalanceAfter),
-    //       Number(new BN(bobBalanceBefore).sub(new BN(price))),
-    //       "bob's balance should be reduced by more than the price of the item (including gas costs)",
-    //     );
-    //   });
+      //     assert.isBelow(
+      //       Number(bobBalanceAfter),
+      //       Number(new BN(bobBalanceBefore).sub(new BN(price))),
+      //       "bob's balance should be reduced by more than the price of the item (including gas costs)",
+      //     );
+      //   });
 
-    //   it("should error when not enough value is sent when purchasing an item", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     await catchRevert(instance.buyItem(0, { from: bob, value: 1 }));
-    //   });
+      //   it("should error when not enough value is sent when purchasing an item", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     await catchRevert(instance.buyItem(0, { from: bob, value: 1 }));
+      //   });
 
-    //   it("should emit LogSold event when and item is purchased", async () => {
-    //     var eventEmitted = false;
+      //   it("should emit LogSold event when and item is purchased", async () => {
+      //     var eventEmitted = false;
 
-    //     await instance.addItem(name, price, { from: alice });
-    //     const tx = await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.addItem(name, price, { from: alice });
+      //     const tx = await instance.buyItem(0, { from: bob, value: excessAmount });
 
-    //     if (tx.logs[0].event == "LogSold") {
-    //       eventEmitted = true;
-    //     }
+      //     if (tx.logs[0].event == "LogSold") {
+      //       eventEmitted = true;
+      //     }
 
-    //     assert.equal(eventEmitted, true, "adding an item should emit a Sold event");
-    //   });
+      //     assert.equal(eventEmitted, true, "adding an item should emit a Sold event");
+      //   });
 
-    //   it("should revert when someone that is not the seller tries to call shipItem()", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: price });
-    //     await catchRevert(instance.shipItem(0, { from: bob }));
-    //   });
+      //   it("should revert when someone that is not the seller tries to call shipItem()", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: price });
+      //     await catchRevert(instance.shipItem(0, { from: bob }));
+      //   });
 
-    //   it("should allow the seller to mark the item as shipped", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
-    //     await instance.shipItem(0, { from: alice });
+      //   it("should allow the seller to mark the item as shipped", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.shipItem(0, { from: alice });
 
-    //     const result = await instance.fetchItem.call(0);
+      //     const result = await instance.fetchItem.call(0);
 
-    //     assert.equal(
-    //       result[3].toString(10),
-    //       SupplyChain.State.Shipped,
-    //       'the state of the item should be "Shipped"',
-    //     );
-    //   });
+      //     assert.equal(
+      //       result[3].toString(10),
+      //       SupplyChain.State.Shipped,
+      //       'the state of the item should be "Shipped"',
+      //     );
+      //   });
 
-    //   it("should emit a LogShipped event when an item is shipped", async () => {
-    //     var eventEmitted = false;
+      //   it("should emit a LogShipped event when an item is shipped", async () => {
+      //     var eventEmitted = false;
 
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
-    //     const tx = await instance.shipItem(0, { from: alice });
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     const tx = await instance.shipItem(0, { from: alice });
 
-    //     if (tx.logs[0].event == "LogShipped") {
-    //       eventEmitted = true;
-    //     }
+      //     if (tx.logs[0].event == "LogShipped") {
+      //       eventEmitted = true;
+      //     }
 
-    //     assert.equal(
-    //       eventEmitted,
-    //       true,
-    //       "adding an item should emit a Shipped event",
-    //     );
-    //   });
+      //     assert.equal(
+      //       eventEmitted,
+      //       true,
+      //       "adding an item should emit a Shipped event",
+      //     );
+      //   });
 
-    //   it("should allow the buyer to mark the item as received", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
-    //     await instance.shipItem(0, { from: alice });
-    //     await instance.receiveItem(0, { from: bob });
+      //   it("should allow the buyer to mark the item as received", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.shipItem(0, { from: alice });
+      //     await instance.receiveItem(0, { from: bob });
 
-    //     const result = await instance.fetchItem.call(0);
+      //     const result = await instance.fetchItem.call(0);
 
-    //     assert.equal(
-    //       result[3].toString(10),
-    //       SupplyChain.State.Received,
-    //       'the state of the item should be "Received"',
-    //     );
-    //   });
+      //     assert.equal(
+      //       result[3].toString(10),
+      //       SupplyChain.State.Received,
+      //       'the state of the item should be "Received"',
+      //     );
+      //   });
 
-    //   it("should revert if an address other than the buyer calls receiveItem()", async () => {
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
-    //     await instance.shipItem(0, { from: alice });
+      //   it("should revert if an address other than the buyer calls receiveItem()", async () => {
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.shipItem(0, { from: alice });
 
-    //     await catchRevert(instance.receiveItem(0, { from: alice }));
-    //   });
+      //     await catchRevert(instance.receiveItem(0, { from: alice }));
+      //   });
 
-    //   it("should emit a LogReceived event when an item is received", async () => {
-    //     var eventEmitted = false;
+      //   it("should emit a LogReceived event when an item is received", async () => {
+      //     var eventEmitted = false;
 
-    //     await instance.addItem(name, price, { from: alice });
-    //     await instance.buyItem(0, { from: bob, value: excessAmount });
-    //     await instance.shipItem(0, { from: alice });
-    //     const tx = await instance.receiveItem(0, { from: bob });
+      //     await instance.addItem(name, price, { from: alice });
+      //     await instance.buyItem(0, { from: bob, value: excessAmount });
+      //     await instance.shipItem(0, { from: alice });
+      //     const tx = await instance.receiveItem(0, { from: bob });
 
-    //     if (tx.logs[0].event == "LogReceived") {
-    //       eventEmitted = true;
-    //     }
+      //     if (tx.logs[0].event == "LogReceived") {
+      //       eventEmitted = true;
+      //     }
 
-    //     assert.equal(
-    //       eventEmitted,
-    //       true,
-    //       "adding an item should emit a Shipped event",
-    //     );
-    //   });
+      //     assert.equal(
+      //       eventEmitted,
+      //       true,
+      //       "adding an item should emit a Shipped event",
+      //     );
+      //   });
+    });
   });
 });
